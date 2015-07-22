f002b74347fd4a1011b7db3b2fa1abb79583c6fd
sss2sssss <sss2sssss@gmail.com>
2015-07-22 20:46:36 +0800

    Start new project

---
+++ IOCon.c
@@ -0,0 +1,9 @@
+#include <xc.h>
+#include "IOCon.h"
+
+void IO_Init()
+{
+	//Assign you pin config at here, example:
+    //TRISBbits.TRISB10 = 0;				// LED1 as output @ RB10/pin21
+
+}
---
+++ Param.h
@@ -0,0 +1,9 @@
+#ifndef PARAM_H
+#define	PARAM_H
+
+//define your pin name at here, example
+// LED1 @ RB10/pin21  // LED2 @ RB11/pin22
+//#define    LED1    LATBbits.LATB10
+//#define    LED2    LATBbits.LATB11
+
+#endif
